fc85fdf77ad5596895a74f16d72b9d83e33595ca
Jeremy Ellis <keyfreemusic@gmail.com>
2019-08-05 20:41:29 -0700

    Update 3.2-check-balance.js

--- code/3.2-check-balance.js
+++ code/3.2-check-balance.js
@@ -15,7 +15,7 @@ iota
   .getBalances([address], 100)
   .then(({ balances }) => {
     console.log(balances)
-   global.myResponse3.2 = "<h2>3.2-check-balance.js</2>" + '<pre id="myPre01">'+JSON.stringify(balances, null, 3)+'</pre>' + "<hr>";  // hopefully this is global
+   global.myResponse32 = "<h2>3.2-check-balance.js</2>" + '<pre id="myPre01">'+JSON.stringify(balances, null, 3)+'</pre>' + "<hr>";  // hopefully this is global
 
   })
   .catch(err => {
