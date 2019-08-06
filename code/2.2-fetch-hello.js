26190e0b8dea4f5736b1086f4a533df19afb994e
Jeremy Ellis <keyfreemusic@gmail.com>
2019-08-05 20:41:52 -0700

    Update 2.2-fetch-hello.js

--- code/2.2-fetch-hello.js
+++ code/2.2-fetch-hello.js
@@ -15,7 +15,7 @@ iota
   .findTransactionObjects({ addresses: [address] })
   .then(response => {
     console.log(response)
-       global.myResponse2.2 = "<h2>2.2-fetch-hello.js</2>" + '<pre id="myPre01">'+JSON.stringify(response, null, 3)+'</pre>' + "<hr>";  // hopefully this is global
+       global.myResponse22 = "<h2>2.2-fetch-hello.js</2>" + '<pre id="myPre01">'+JSON.stringify(response, null, 3)+'</pre>' + "<hr>";  // hopefully this is global
 
   })
   .catch(err => {
