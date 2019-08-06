616f3d04845c6ad4b7b642591bb75dc7e4e6150c
Jeremy Ellis <keyfreemusic@gmail.com>
2019-08-05 20:31:51 -0700

    Update 1-hello-world.js

--- code/1-hello-world.js
+++ code/1-hello-world.js
@@ -11,9 +11,9 @@ const iota = iotaLibrary.composeAPI({
 iota
   .getNodeInfo()
   .then(response => {
-  
+
       console.log(response)
-      global.myResponse1 = response;  // hopefully this is global                 
+      global.myResponse1 = "<h2>1-hello-world.js</2>" + '<pre id="myPre01">'+JSON.stringify(response, null, 3)+'</pre>' + "<hr>";  // hopefully this is global
    })
   .catch(err => {
     console.error(err)
